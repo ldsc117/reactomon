4ef7e12a804cb5e6668c2c0de931e8a193ed8af2
ldsc117 <62885313+ldsc117@users.noreply.github.com>
2021-03-21 05:56:20 +0200

    Fetched PokemonList from api

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,8 +1,30 @@
+import React , {useState , useEffect} from 'react';
+
 
 function PokemonList() {
+
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const [items,setItems] = useState([]);
+
+    const fetchItems = async() =>{
+        const data = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+        
+        const items = await data.json();
+        console.log(items.results);
+        setItems(items.results);
+    } 
+
+
   return (
     <div>
       <h6>PokemonList</h6>
+      {items.map(item => (
+          <h1>{item.name}</h1>
+      ))}
     </div>
   );
 }
