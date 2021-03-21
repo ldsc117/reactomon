20c20ff6bad9d50ae320514407ca720d7879088d
ldsc117 <62885313+ldsc117@users.noreply.github.com>
2021-03-21 04:18:31 +0200

    Finished the routes for the main page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import NavBar from './components/NavBar';
 import PokemonDetail from './components/PokemonDetail';
 import PokemonList from './components/PokemonList';
 import TypeList from './components/TypeList';
+import { BrowserRouter as Router, Switch , Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <NavBar />
-      <PokemonDetail />
-      <PokemonList />
-      {/* <TypeList />  */}
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/pokemons" component={PokemonList}/>
+      {/* <PokemonDetail /> */}
+     
+      <Route path="/types" component={TypeList} /> 
+      </Switch>
+      </Router>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
 
 export default App;
