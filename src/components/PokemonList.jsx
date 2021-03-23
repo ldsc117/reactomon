5fcf55e91f2301495b452be9b76ea2ede347f952
ldsc117 <62885313+ldsc117@users.noreply.github.com>
2021-03-23 16:53:54 +0200

    Added buttons for changing pages for PokemonList

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -6,19 +6,36 @@ function PokemonList() {
 
     useEffect(() => {
         fetchItems();
-    }, []);
+    });
 
     const [items,setItems] = useState([]);
+    const [offset,setOffset] = useState(0);
+    const [pageNumber,setPageNumber] = useState(1);
+    
 
     const fetchItems = async() =>{
-        const data = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`);
         
         const items = await data.json();
         console.log(items.results);
         setItems(items.results);
     } 
     // const [count ,setCount] = useState(1);
-
+    function nextClick(e) {
+        e.preventDefault();
+        setOffset(offset+20);
+        setPageNumber(pageNumber+1);
+        console.log('Clocked');
+    }
+    function previousClick(e) {
+        e.preventDefault();
+        if(offset>=20){
+            setOffset(offset-20);
+            setPageNumber(pageNumber-1);
+        }
+        
+        console.log('Clocked');
+    }
 
   return (
     <div>
@@ -27,12 +44,15 @@ function PokemonList() {
            
           <h1 key = {index+1}>
               
-              <Link to={`/pokemons/${index+1}`}>{pokemon.name}</Link>
+              <Link to={`/pokemons/${index+1+((pageNumber-1) * 20 )}`}>{pokemon.name}</Link>
               
               </h1>
 
           
       ))}
+      <h4>Page Number: {pageNumber}</h4>
+      <button onClick={previousClick}>Previous Page</button>
+      <button onClick={nextClick}>Next Page</button>
     </div>
   );
 }
